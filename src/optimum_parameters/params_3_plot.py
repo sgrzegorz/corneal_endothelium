2236429e1d7bce077bad186b44322258715d300c
sgrzegorz <grzegorzsroka2@gmail.com>
2023-06-20 12:09:10 +0200

    Fix

---
+++ src/optimum_parameters/params_3_plot.py
@@ -0,0 +1,38 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+from src.utils import load_dataset, path_root
+
+
+
+def plot_unknown_3(name,filtering):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    df = load_dataset(name,filtering)
+
+    # x = np.random.standard_normal(100)
+    # y = np.random.standard_normal(100)
+    # z = np.random.standard_normal(100)
+    # c = np.random.standard_normal(100)
+
+    img = ax.scatter(df.r, df.param1, df.param2, c=df.fitness, cmap='bwr')
+    ax.set_xlabel('r')
+    ax.set_ylabel('param1')
+    ax.set_zlabel('param2')
+    maximum = df[df.fitness == df.fitness.max()]
+    ax.scatter(maximum.r, maximum.param1, maximum.param2, color='black', s=85, label='maximum')
+    fig.colorbar(img)
+
+    title = f'{filtering}_{name}_4d'
+    plt.title(title)
+    plt.legend()
+
+    plt.savefig(path_root('plots', 'optimum', title))
+    plt.show()
+
+
+for filtering in ['yg','bs','jgs']:
+    for name in ['phansalkar','niblack','sauvola']:
+        plot_unknown_3(name, filtering)
